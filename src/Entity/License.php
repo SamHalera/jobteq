b3b07c9cb29bab55f390e9d69ec625f919163bff
SamHalera <samuel.halera@gmail.com>
2025-01-16 15:47:42 +0100

    create entity License and LicenseConfig

---
+++ src/Entity/License.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LicenseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LicenseRepository::class)]
+class License
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $credits = null;
+
+    #[ORM\ManyToOne(inversedBy: 'licenses')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?LicenseConfig $licenseConfig = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCredits(): ?int
+    {
+        return $this->credits;
+    }
+
+    public function setCredits(int $credits): static
+    {
+        $this->credits = $credits;
+
+        return $this;
+    }
+
+    public function getLicenseConfig(): ?LicenseConfig
+    {
+        return $this->licenseConfig;
+    }
+
+    public function setLicenseConfig(?LicenseConfig $licenseConfig): static
+    {
+        $this->licenseConfig = $licenseConfig;
+
+        return $this;
+    }
+}
