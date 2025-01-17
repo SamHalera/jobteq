1cf3834dbdebba87c19b5ed35157629581a2d1fa
SamHalera <samuel.halera@gmail.com>
2025-01-17 22:42:44 +0100

    edit invitation

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -48,7 +48,7 @@ class ManagerController extends AbstractController
                 ->setToken($uid)
                 ->setCreatedBy($this->getUser())
                 ->setStatus(InvitationStatusEnum::PENDING)
-                ->setSendings($sendings + 1)
+                ->setSendings(1)
             ;
 
             $em->persist($invitation);
@@ -76,12 +76,27 @@ class ManagerController extends AbstractController
         return $this->redirectToRoute('app_manager_invitation_index');
     }
     #[Route('/invitations/edit/{id}', name: 'app_manager_invitation_edit')]
-    public function invitationEdit(Invitation $invitation, EntityManagerInterface $em): Response
+    public function invitationEdit(Invitation $invitation, EntityManagerInterface $em, Request $request): Response
     {
 
+        $form = $this->createForm(InvitationType::class, $invitation);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-        return $this->redirectToRoute('app_manager_invitation_index');
+            $uid = Uuid::v7();
+            $invitation
+                ->setToken($uid);
+
+            $em->persist($invitation);
+            $em->flush();
+
+            return $this->redirectToRoute('app_manager_invitation_index');
+        }
+
+        return $this->render('manager/edit-invitation.html.twig', [
+            'form' => $form
+        ]);
     }
     #[Route('/invitations/re-send-email/{id}', name: 'app_manager_invitation_resend_email')]
     public function invitationResendEmail(Invitation $invitation, EntityManagerInterface $em): Response
